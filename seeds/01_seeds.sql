e999d92e2d53062e5b72226f7cd0f9922b3f6cdb
Your Name <you@example.com>
2021-04-01 22:10:03 +0000

    fix errors and test reservations

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -10,9 +10,9 @@ VALUES ('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/
 ('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, song_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUES (1, 'Alsajee Estate Villas', 'description', 'https://www.me.com/en-ca/cottage-rental/p1011512vb?noDates=true', 'https://www.me.com/en-ca/cottage-rental/p1011512vb?noDates=true', 1800, 4, 5, 5, 'Canada', 'Cowan Rd', 'West Vancouver', 'British Columbia', 'V6G 2G4', TRUE),
-(2, 'Farouk Palace', 'description', 'https://www.me.com/6651340ha?noDates=true', 'https://www.me.com/6653140ha?noDates=true', 200, 2, 2, 1, 'Iraq', 'Mansour', 'Baghdad', 'Baghdad', '122456', TRUE),
-(3, 'Frankeys Apartment', 'description', 'https://me.co.jp/rent/view/301691', 'https://me.co.jp/rent/view/301619', 300, 1, 1, 1, 'UAE', 'Barasti', 'Dubai', 'Almakan', 'A1S4S1', TRUE);
+VALUES (1, 'Alsajee Estate Villas', 'description', 'https://www.cottagesincanada.com/19102', 'https://www.cottagesincanada.com/19102', 1800, 4, 5, 5, 'Canada', 'Cowan Rd', 'West Vancouver', 'British Columbia', 'V6G 2G4', TRUE),
+(2, 'Farouk Palace', 'description', 'https://www.cottagesincanada.com/21016', 'https://www.cottagesincanada.com/21016', 200, 2, 2, 1, 'Iraq', 'Mansour', 'Baghdad', 'Baghdad', '122456', TRUE),
+(3, 'Frankeys Apartment', 'description', 'https://www.cottagesincanada.com/31147', 'https://www.cottagesincanada.com/31147', 300, 1, 1, 1, 'UAE', 'Barasti', 'Dubai', 'Almakan', 'A1S4S1', TRUE);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2019-01-01', '2020-01-01', 1, 2),
