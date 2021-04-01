1f4925daa345a564602f4d52f7fd9ea01c76ea61
Your Name <you@example.com>
2021-04-01 03:10:24 +0000

    add seeds 2 and run against Lightbnb

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,11 +3,10 @@ DELETE FROM properties;
 DELETE FROM reservations;
 DELETE FROM property_reviews;
 */
-
 INSERT INTO users (name, email, password)
-VALUES ('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+VALUES ('Farouk', 'faroukalsajee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Farouk', 'faroukalsajee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Farouk', 'faroukalsajee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (artist_id, title, description, song_url, music_genre)
 VALUES (1, 'Looti', 'description', 'https://www.youtube.com/watch?v=vx2cV1gznk8', 'world'),
