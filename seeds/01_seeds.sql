8f18f2899a9af6ca46dac52ff3843106c4e118e5
Your Name <you@example.com>
2021-04-01 03:41:38 +0000

    add all queries 1 files

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,22 +3,23 @@ DELETE FROM properties;
 DELETE FROM reservations;
 DELETE FROM property_reviews;
 */
+
 INSERT INTO users (name, email, password)
-VALUES ('Farouk', 'faroukalsajee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Farouk', 'faroukalsajee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-('Farouk', 'faroukalsajee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+VALUES ('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Farouk', 'faroukalsajee@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties (artist_id, title, description, song_url, music_genre)
-VALUES (1, 'Looti', 'description', 'https://www.youtube.com/watch?v=vx2cV1gznk8', 'world'),
-(2, 'Winsa', 'description', 'https://www.youtube.com/watch?v=aNNLren1_xc', 'world'),
-(3, 'Habitak', 'description', 'https://www.youtube.com/watch?v=tLnwYiQOFIM', 'world');
+INSERT INTO properties (owner_id, title, description, song_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Alsajee Estate Villas', 'description', 'https://www.me.com/en-ca/cottage-rental/p1011512vb?noDates=true', 'https://www.me.com/en-ca/cottage-rental/p1011512vb?noDates=true', 1800, 4, 5, 5, 'Canada', 'Cowan Rd', 'West Vancouver', 'British Columbia', 'V6G 2G4', TRUE),
+(2, 'Farouk Palace', 'description', 'https://www.me.com/6651340ha?noDates=true', 'https://www.me.com/6653140ha?noDates=true', 200, 2, 2, 1, 'Iraq', 'Mansour', 'Baghdad', 'Baghdad', '122456', TRUE),
+(3, 'Frankeys Apartment', 'description', 'https://me.co.jp/rent/view/301691', 'https://me.co.jp/rent/view/301619', 300, 1, 1, 1, 'UAE', 'Barasti', 'Dubai', 'Almakan', 'A1S4S1', TRUE);
 
-INSERT INTO details (release_date, album_numebr)
-VALUES ('2018', 1),
-('2018', 1),
-('2018', 1);
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2019-01-01', '2020-01-01', 1, 2),
+('2019-01-01', '2022-01-01', 2, 3),
+('2019-01-01', '2030-01-01', 3, 1);
 
-INSERT INTO orders (label_id, song_order, rating, message)
-VALUES (1, 2, 1, 'message'),
-(2, 3, 2, 'message'),
-(5, 3, 3, 'message');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (2, 1, 1, 3, 'message'),
+(3, 2, 2, 5, 'message'),
+(1, 3, 3, 5, 'message');
